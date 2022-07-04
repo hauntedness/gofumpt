0acd60b3dee2db13e545d75b0740715dbaa151d4
huantedness <keikoamo@163.com>
2022-07-04 21:33:14 +0800

    fix nested if {}

--- sss.go
+++ sss.go
@@ -6,6 +6,16 @@ import (
 
 func test() error {
 	var err error
-	if err != nil { return errors.WithMessage(err, "") }
+
+	if err != nil {
+		if err != nil {
+			if err != nil { return errors.WithMessage(err, "") }
+			return errors.WithMessage(err, "")
+		}
+		if err != nil { return errors.WithMessage(err, "") }
+	}
+	if err != nil {
+		return errors.WithMessage(err, "") //
+	}
 	return nil
 }
